d6934a5c19826b6632637fc3323284f6d37f4074
Scott <sfitzpatrick55@gmail.com>
2022-04-21 16:43:37 -0400

    Update RRoll link

--- easterEgg.js
+++ easterEgg.js
@@ -5,7 +5,7 @@ for (let element = 0; element < profilePics.length; element ++) {
     addEventListener("click", function() {
         counter ++;
         if (counter >= 6) {
-            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+            window.open("https://youtube.com/clip/UgkxxjvuSZVthrEBCzGVc3pRQazHWPRoYpOa", "_blank")
             counter = 0;
         }
     })
